99857fb3871ada11f9513ad3f68b623d1b296985
Alex Miner <alex.miner@gmail.com>
2018-12-02 23:35:25 -0800

    something ok

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 var movies = [
   {title: 'Mean Girls'},
   {title: 'Hackers'},
@@ -7,34 +9,96 @@ var movies = [
 ];
 
 
-var MovieItem = (props) => (
-	
-		<li>
-			<div className="movie-list-entry">
-				{props.movie.title}
-			</div>
-		</li>
+class Search extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {query: ''};
+		this.handleInputChange = this.handleInputChange.bind(this)
+	}
 
-)
+	handleInputChange(event) {
+		
+		this.setState({
+     		query: event.target.value
+   		});
+
+   		this.props.handleSearchHandler(event);
+   		
+ 	}
+
+ 	render() {
+ 		return (
+ 			<form>
+       			<input placeholder="Search for..." value={this.state.query} onChange={this.handleInputChange} />
+       			
+     		</form>
+ 		)
+ 	}
+
+}
+
+class MovieItem extends React.Component {
+	constructor(props) {
+		super(props);
+
+	}
+	render () {
+		return (
+			<li>
+				<div className="movie-list-entry">
+					{this.props.movie.title}
+				</div>
+			</li>
+		)
+	}
+}
 
 var MovieList = (props) => (
 	<ul>
 		 {
-		 	props.movies.map(movie => <MovieItem movie={movie} />)
+		 	props.movies.filter(movie => movie.title.indexOf(props.query) >= 0).map(movie => <MovieItem movie={movie} />)
 		 }
 	</ul>
 )
 
 
-var App = (props) => (
-	<div>
-		<h1 className="test">Movie List</h1>
-
-			<MovieList movies={props.movies} />
-
-	</div>
-)
 
 
+class App extends React.Component {
 
-ReactDOM.render(<App movies={movies}/>, document.getElementById('app'))
+	constructor(props) {
+		super(props);
+		this.state = {
+			query: '',
+			movies: [
+			  {title: 'Mean Girls'},
+			  {title: 'Hackers'},
+			  {title: 'The Grey'},
+			  {title: 'Sunshine'},
+			  {title: 'Ex Machina'},
+			]
+		};
+
+		this.handleSearchHandler = this.handleSearchHandler.bind(this);
+	}
+
+	handleSearchHandler(event) {	
+		console.log(event.target.value)	;
+		this.setState({
+     		query: event.target.value
+   		});
+ 	}
+
+	render() {
+		return (
+			<div>
+				<h1 className="test">Movie List</h1>
+				<Search query={this.state.query} handleSearchHandler={this.handleSearchHandler}/>
+				<MovieList movies={this.state.movies} query={this.state.query}/>
+ 			</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
+
